c488978efad8137f21ea75b14e0bda3df4d1bfa1
Byron <byrontan92@gmail.com>
2021-09-08 13:18:12 +0800

    Submission
    removed unused import

--- src/api.js
+++ src/api.js
@@ -1,14 +1,64 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const Drone = require("./drone");
 
+const app = express();
 
 app.use(cors());
+// Probably a better way to do this but added limit to handle PayloadTooLargeError
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', (req, res) => {
-    res.json({foo: 'bar'});
+app.get("/", (req, res) => {
+  res.json({ foo: "bar" });
+});
+
+app.post("/singleDroneInput", (req, res) => {
+  const input = req.body.input;
+  const output = droneReader(input);
+  res.json({ photos: output });
+});
+
+app.post("/twoDroneInput", (req, res) => {
+  const input = req.body.input;
+  const output = twoDroneReader(input);
+  res.json({ photos: output });
 });
 
 app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
 
+function droneReader(droneInput) {
+  commandArray = [...droneInput];
+  const drone = new Drone();
+
+  streamCommands(drone, commandArray);
+
+  return drone.getNumberOfPhotos();
+}
+
+function twoDroneReader(dronesInput) {
+  commandArray = [...dronesInput];
+
+  droneOneCommandArray = [];
+  droneTwoCommandArray = [];
+  partitionedCommandArray = [droneOneCommandArray, droneTwoCommandArray];
+
+  commandArray.forEach((command, index) =>
+    partitionedCommandArray[index % 2].push(command)
+  );
+
+  const droneOne = new Drone();
+  streamCommands(droneOne, droneOneCommandArray);
+
+  const droneTwo = new Drone();
+  droneTwo.setSnapshotStorage(droneOne.getSnapshotStorage());
+  streamCommands(droneTwo, droneTwoCommandArray);
+
+  return droneTwo.getNumberOfPhotos();
+}
+
+function streamCommands(drone, commandArray) {
+  for (let index = 0; index < commandArray.length; index++) {
+    const command = commandArray[index];
+    drone.commandHandler(command);
+  }
+}
--- src/client.js
+++ src/client.js
@@ -1,15 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import useFetch from 'react-fetch-hook';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import FormLabel from "@material-ui/core/FormLabel";
+import Button from "@material-ui/core/Button";
+import Switch from "@material-ui/core/Switch";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
-    const {isLoading, data} = useFetch('http://localhost:4001');
-    if(isLoading) {
-        return 'Loading...';
+  const classes = useStyles();
+  const [commands, setCommands] = useState();
+  const [twoDroneToggle, setTwoDroneToggle] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const requestBody = {
+      input: commands,
+    };
+
+    var requestURL = "";
+
+    if (twoDroneToggle == true) {
+      requestURL = "http://localhost:4001/twoDroneInput";
+    } else {
+      requestURL = "http://localhost:4001/singleDroneInput";
     }
-    return <pre>{JSON.stringify(data, null, 4)}</pre>;
+
+    axios.post(requestURL, requestBody).then((res) => {
+      if (res.status === 200) {
+        alert("Success!, there were " + res.data.photos + " photos taken");
+      }
+    });
+  };
+
+  return (
+    <div className={classes.root}>
+      <form onSubmit={handleSubmit}>
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12}>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Insert Drone Input Here"
+              multiline
+              required
+              fullWidth
+              rows={20}
+              value={commands}
+              onChange={(e) => setCommands(e.target.value)}
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item>
+            <FormLabel>Single Drone</FormLabel>
+          </Grid>
+          <Grid item>
+            <Switch
+              onChange={(e) => setTwoDroneToggle(!twoDroneToggle)}
+              value={twoDroneToggle}
+              name="checkedB"
+              color="primary"
+            />
+          </Grid>
+          <Grid item>
+            <FormLabel>Two Drones</FormLabel>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary" type="submit">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
 }
 
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
