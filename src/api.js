bf260b9b9cf4ddf90d2bb7518c51506d6412953d
Arvin Kenn delos Santos <adsantos.417@gmail.com>
2022-02-27 12:23:51 +0800

    API add error handling

--- src/api.js
+++ src/api.js
@@ -6,6 +6,7 @@ const { processInstructions } = require("./services/DroneService");
 const { INSTRUCTIONS } = require("./constants/instruction");
 
 app.use(cors());
+// fix for payload too large
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
@@ -13,20 +14,27 @@ app.get("/", (req, res) => {
 });
 
 app.post("/instructions", (req, res) => {
-  const { instructions, drones } = req.body;
+  try {
+    const { instructions, drones } = req.body;
 
-  const droneInstructions = instructions.split("").filter((instruction) => {
-    return [
-      INSTRUCTIONS["X"],
-      INSTRUCTIONS["N"],
-      INSTRUCTIONS["S"],
-      INSTRUCTIONS["E"],
-      INSTRUCTIONS["W"],
-    ].includes(instruction);
-  });
+    const droneInstructions = instructions.split("").filter((instruction) => {
+      return [
+        INSTRUCTIONS["X"],
+        INSTRUCTIONS["N"],
+        INSTRUCTIONS["S"],
+        INSTRUCTIONS["E"],
+        INSTRUCTIONS["W"],
+      ].includes(instruction);
+    });
 
-  const data = processInstructions(droneInstructions, drones);
-  res.json({ data: data });
+    const data = processInstructions(droneInstructions, drones);
+    res.json({ data: data });
+  } catch (error) {
+    res.status(500).json({
+      error: 1,
+      message: error.message,
+    });
+  }
 });
 
 app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
